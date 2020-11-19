6f008b8fd3f27bb22c459bf19a802a0e4568aa66
Salahuddin <salahuddinshaikh16@gmail.com>
2020-11-19 17:49:48 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/test_your_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.salahuddinshaikh.test_your_apptest_your_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+// import 'package:store_redirect/store_redirect.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+final usersRef = Firestore.instance.collection('users');
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Test your app',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Test your app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+     usersRef.document('users').setData({
+                  "timestamp" : DateTime.now()
+                });
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have deleted the file this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            RaisedButton(
+              child: Text("Redirect app"),
+              onPressed: () {
+               
+                // StoreRedirect.redirect(
+                //   androidAppId: "org.mozilla.firefox"
+                // );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Decrement',
+        child: Icon(Icons.delete_forever),
+      ),
+    );
+  }
+}
